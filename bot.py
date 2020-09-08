cff6ad8278279db29972505d1120bd593e393a8b
jonduperron_bespin <jonathan.du_perron@us.af.mil>
2020-09-08 12:30:25 -0500

    ~switched apis +added current price look up

--- bot.py
+++ bot.py
@@ -6,18 +6,26 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 import json
-
 load_dotenv()
+
 TOKEN = os.getenv('DISCORD_TOKEN')
 API = os.getenv('API_TOKEN')
+
 bot = commands.Bot(command_prefix='$')
 
-@bot.command()
-async def end(ctx, arg1):
-    url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={}&outputsize=compact&apikey={}'.format(arg1, API)
-    request = requests.get(url)
+@bot.command(
+help="This returns the current quote for the requested stock. To use this command type '$current <Stock Ticker>'",
+brief="This returns the current quote for the requested stock."
+)
+async def current(ctx, arg1): #TODO: add error handling
+
+    url = 'https://finnhub.io/api/v1/quote?symbol={}&token={}'.format(arg1, API)
+    request = requests.get(url, stream=True)
     response = request.text
-    decoded_response = json.JSONDecoder().decode(response)
-    await ctx.send(decoded_response)
+    decoded_response = json.loads(response)
+    current_price = (decoded_response['c'])
+    await ctx.send('The current price of {} is: {}'.format(arg1, current_price))
 
 bot.run(TOKEN)
+
+#hot desk swap wright pat
