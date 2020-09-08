38ab42d2354f007057a7d24e1fbda64790f78e14
Jonathan Du Perron <jonathan@duperron.io>
2020-09-08 14:00:40 -0500

    ~working on error handling

--- bot.py
+++ bot.py
@@ -11,8 +11,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 API = os.getenv('API_TOKEN')
 
 bot = commands.Bot(command_prefix='$')
-BASIC_ARG = "empty"
-DEFAULT_ARG = BASIC_ARG.format("str", "string")
+DEFAULT_ARG = "empty"
 
 #current price
 @bot.command(
@@ -23,12 +22,16 @@ brief="This returns the current quote for the requested stock."
 async def current(ctx, arg1=DEFAULT_ARG): #TODO: add error handling.
 
     if arg1 != 'empty':
-        url = 'https://finnhub.io/api/v1/quote?symbol={}&token={}'.format(API)
+        url = 'https://finnhub.io/api/v1/quote?symbol={}&token={}'.format(arg1, API)
         request = requests.get(url)
         response = request.text
+        print(response)
         decoded_response = json.loads(response)
-        current_price = (decoded_response['c'])
-        print(current_price)
+        try:
+            current_price = (decoded_response['c'])
+            print(current_price)
+        except KeyError:
+            print("potato")
         await ctx.send('The current price of {} is: {}'.format(arg1, current_price))
     else:
         await ctx.send('You need to include at least one ticker to your command.')
