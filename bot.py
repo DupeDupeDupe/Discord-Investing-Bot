5b2fb807342b8cc469b62bf1c6ef72bc24bf93d4
Jonathan Du Perron <jonathan@duperron.io>
2020-09-08 13:00:31 -0500

    +Added daily lows, daily highs and opening prices

--- bot.py
+++ bot.py
@@ -12,6 +12,7 @@ API = os.getenv('API_TOKEN')
 
 bot = commands.Bot(command_prefix='$')
 
+#current price
 @bot.command(
 help="This returns the current quote for the requested stock. To use this command type '$current <Stock Ticker>'",
 brief="This returns the current quote for the requested stock."
@@ -24,5 +25,47 @@ async def current(ctx, arg1): #TODO: add error handling.
     decoded_response = json.loads(response)
     current_price = (decoded_response['c'])
     await ctx.send('The current price of {} is: {}'.format(arg1, current_price))
+
+#daily low
+@bot.command(
+help="This returns the daily low for the requested stock. To use this command type '$dl <Stock Ticker>'",
+brief="This returns the daily low for the requested stock."
+)
+async def dl(ctx, arg1): #TODO: add error handling.
+
+    url = 'https://finnhub.io/api/v1/quote?symbol={}&token={}'.format(arg1, API)
+    request = requests.get(url)
+    response = request.text
+    decoded_response = json.loads(response)
+    daily_low = (decoded_response['l'])
+    await ctx.send('The daily low of {} is: {}'.format(arg1, daily_low))
+
+#daily high
+@bot.command(
+help="This returns the daily high for the requested stock. To use this command type '$dh <Stock Ticker>'",
+brief="This returns the daily high for the requested stock."
+)
+async def dh(ctx, arg1): #TODO: add error handling.
+
+    url = 'https://finnhub.io/api/v1/quote?symbol={}&token={}'.format(arg1, API)
+    request = requests.get(url)
+    response = request.text
+    decoded_response = json.loads(response)
+    daily_high = (decoded_response['h'])
+    await ctx.send('The daily high of {} is: {}'.format(arg1, daily_high))
+
+#open
+@bot.command(
+help="This returns the opening price for the requested stock. To use this command type '$open <Stock Ticker>'",
+brief="This returns the opening price for the requested stock."
+)
+async def open(ctx, arg1): #TODO: add error handling.
+
+    url = 'https://finnhub.io/api/v1/quote?symbol={}&token={}'.format(arg1, API)
+    request = requests.get(url)
+    response = request.text
+    decoded_response = json.loads(response)
+    opened = (decoded_response['o'])
+    await ctx.send('{} opened at: {}'.format(arg1, opened))
 
 bot.run(TOKEN)
